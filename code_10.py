637b7e49dbe062441afd25b69b3d7cc8a5960f73
Ricklovin <123012156+Ricklovin@users.noreply.github.com>
2023-04-10 13:27:53 -0600

    update

--- code_10.py
+++ code_10.py
@@ -1,1 +1,20 @@
-import nltk as nk
+import ssl
+import nltk 
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+nltk.download('punkt')
+
+reviews = open('tacos_reviews.txt')
+
+for review in reviews:
+    tokens = nltk.word_tokenize(review)
+    print(tokens)
+
+    pos_tags = nltk.pos_tag(tokens)
+    print(pos_tags)
+
+    for token in tokens:
+        if token[1] == 'JJ' or token[1] == 'JJS':
+         #print(token[0])
+         print(token)
